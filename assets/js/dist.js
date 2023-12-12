9b8b1fd057bf04ece70dc978fbacc8c37d121d84
peterchaotw <peter.chao.forjob@gmail.com>
2023-12-12 18:35:47 +0900

    init

--- assets/js/dist.js
+++
@@ -1,8 +0,0 @@
-function Welcome( props ) {
-    return React.createElement( 'div', null, 'Hello World' );
-}
-
-ReactDOM.render(
-    React.createElement( Welcome, {}, null ),
-    document.getElementById( 'app' )
-);
