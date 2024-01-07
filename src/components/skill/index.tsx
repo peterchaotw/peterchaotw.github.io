bbf5e9d9b4f9fdeedbb4f7a57eb8a6897fe5512c
peterchaotw <peter.chao.forjob@gmail.com>
2024-01-07 21:42:51 +0900

    ver1.2

--- src/components/skill/index.tsx
+++ src/components/skill/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable array-callback-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
